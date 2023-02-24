378002231026390c0dd5e67f9d473b7fd8105ccc
choijeaho86@gmail.com <choijeaho86@gmail.com>
2023-02-24 14:52:16 +0900

    connect redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-redis-store';
 import { typeOrmConfig } from 'config/typeorm.config';
+import { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './member/member.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), MemberModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MemberModule,
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      url: process.env.REDIS_URL,
+      isGlobal: true, // 모든 모듈에서 사용 가능하도록 설정
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
